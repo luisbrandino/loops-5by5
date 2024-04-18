f3925aec0c4fb2587a8e3a87d758e0a2ad93d26b
luisbrandino <luisbrandino.contato@gmail.com>
2024-04-18 16:04:48 -0300

    Add project files.

---
+++ Exercicio4/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Digite um número para o início: ");
+int start = int.Parse(Console.ReadLine());
+
+Console.Write("Digite um número para o incremento: ");
+int increment = int.Parse(Console.ReadLine());
+
+int i = 0;
+
+for (; i < increment; i++)
+    Console.WriteLine($"For: {start + i}");
+
+Console.WriteLine($"Número final depois do for: {start + i}");
+
+i = 0;
+do
+{
+    Console.WriteLine($"Do while: {start + i}");
+    i++;
+} while (i < increment);
+
+Console.WriteLine($"Número final depois do do while: {start + i}");
+
+i = 0;
+while (i < increment)
+{
+    Console.WriteLine($"While: {start + i}");
+    i++;
+}
+
+Console.WriteLine($"Número final depois do while: {start + i}");
